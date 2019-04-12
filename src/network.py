3f2b7b99483af83463eb007ea98bc402d2e93139
oisin-dolphin <oisin@your.md>
2019-04-12 16:59:42 +0100

    duel dqn!

--- src/network.py
+++ src/network.py
@@ -6,8 +6,8 @@ import torch.nn.functional as F
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed=0, fc1_units=256,
-                 fc2_units=256):
+    def __init__(self, state_size, action_size, seed=0, fc1_units=64,
+                 fc2_units=32):
         """Initialize parameters and build model.
         Params
         ======
@@ -23,8 +23,10 @@ class QNetwork(nn.Module):
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
 
+        self.value_estimate = nn.Linear(fc2_units, 1)
+
     def forward(self, state):
         """Build a network that maps state -> action values."""
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
-        return self.fc3(x)
+        return self.fc3(x) + self.value_estimate(x)
